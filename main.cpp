a383737b660ac2e8134a723765b5cd59c4cdeba9
EternalFir <3480452528@qq.com>
2022-01-08 21:54:41 +0800

    Ignored The Not Required File Holder

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main() {
     int times = 0;
     while (getline(std::cin, command_in)) {
         times++;
-        if (times == 10) {
+        if (times == 20) {
             times = 0;
             account_management.AutoSave();
             book_management.AutoSave();
